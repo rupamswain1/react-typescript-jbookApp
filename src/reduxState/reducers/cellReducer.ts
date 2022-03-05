c9e5856eabb18ed8483c67118db6bf159da7df49
rupamswain1 <rupamswain1@gmail.com>
2022-03-06 01:22:18 +0530

    immer added for reducer

--- src/reduxState/reducers/cellReducer.ts
+++ src/reduxState/reducers/cellReducer.ts
@@ -1,6 +1,7 @@
 import { ActionType } from '../action-types'
 import { Action } from '../actions'
 import { Cell } from '../cell'
+import { produce } from 'immer'
 
 interface CellState {
   loading: boolean
@@ -18,30 +19,35 @@ const InitialState: CellState = {
   data: {},
 }
 
-const CellReducer = (
-  state: CellState = InitialState,
-  action: Action,
-): CellState => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload
-      return {
-        ...state,
-        [id]: {
-          ...state.data[id],
-          content,
-        },
-      }
-    case ActionType.DELETE_CELL:
-      return state
-    case ActionType.MOVE_CELL:
-      return state
-    case ActionType.INSERT_CELL_BEFORE:
-      return state
-    default:
-      return state
-  }
-  return state
-}
+const CellReducer = produce(
+  (state: CellState = InitialState, action: Action): CellState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload
+        state.data[id].content = content
+        return state
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload]
+        state.order = state.order.filter((id) => id !== action.payload)
+        return state
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload
+        const index = state.order.findIndex((id) => id === action.payload.id)
+        const targetIndex = direction === 'up' ? index - 1 : index + 1
+
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return state
+        }
+        state.order[index] = state.order[targetIndex]
+        state.order[targetIndex] = action.payload.id
+        return state
+      case ActionType.INSERT_CELL_BEFORE:
+        return state
+      default:
+        return state
+    }
+    return state
+  },
+)
 
 export default CellReducer
