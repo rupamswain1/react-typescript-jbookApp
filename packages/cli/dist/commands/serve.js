66c9ba20a0f61c3fd081c4ca8bc1de807e91804a
rupamswain1 <rupamswain1@gmail.com>
2022-04-13 00:56:20 +0530

    local api vreation

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,12 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const commander_1 = require("commander");
 const local_api_1 = require("local-api");
+const path_1 = __importDefault(require("path"));
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run serve on', '4000')
-    .action((filename = 'notebook.js', options) => {
-    (0, local_api_1.serve)(parseInt(options.port), filename, '/');
-});
+    .action((filename = 'notebook.js', options) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+        yield (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir);
+    }
+    catch (err) {
+        console.log('hereeeee is the error', err.message);
+    }
+}));
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,15 @@
 import { Command } from 'commander'
 import { serve } from 'local-api'
+import path from 'path'
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run serve on', '4000')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    serve(parseInt(options.port), filename, '/')
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename))
+      await serve(parseInt(options.port), path.basename(filename), dir)
+    } catch (err) {
+      console.log('hereeeee is the error', err.message)
+    }
   })
